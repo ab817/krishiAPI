80adc5189f266410cc5c60d9763f9f872f6eea2e
thapaa9 <pashupati.thapa9@gmail.com>
2026-02-11 21:53:55 +0545

    Initial commit

--- krishi_backend/settings.py
+++ krishi_backend/settings.py
@@ -97,11 +97,11 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT',),  # Default PostgreSQL port
+        'NAME': 'krishi_db',          # Your database name
+        'USER': 'pashupati',               # Your MySQL username
+        'PASSWORD': 'myname',  # Your MySQL password
+        'HOST': '127.0.0.1',          # Usually localhost
+        'PORT': '3306',               # Default MySQL port
     }
 }
 
